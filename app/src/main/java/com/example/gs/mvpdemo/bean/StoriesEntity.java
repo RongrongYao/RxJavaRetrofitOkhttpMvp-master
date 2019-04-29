56151bdadce954206cc894c3bbc85470813cb7f7
yao <18562317868@163.com>
2019-04-29 12:53:37 +0800

    1

---
+++ app/src/main/java/com/example/gs/mvpdemo/bean/StoriesEntity.java
@@ -0,0 +1,23 @@
+package com.example.gs.mvpdemo.bean;
+
+public class StoriesEntity {
+
+    private int type;
+    private int pic;
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getPic() {
+        return pic;
+    }
+
+    public void setPic(int pic) {
+        this.pic = pic;
+    }
+}
